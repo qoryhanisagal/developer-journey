705f1055fd40385aa695b52fb086fb504964f51a
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-02-06 10:19:51 -0600

    Add JavaScript funny story Create Story Section script

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+// -- Funny Story ---
+//Declare variable using let and constant
+let character = "Koiree";
+let action = "debugged";
+let object = "a bug size of Mount Everest";
+let place = "the coffee shop";
+let time = "a stormy night";
+
+const occasion = "the final bug in the project";
+
+// Create the story
+console.log("--- Funny Story ---");
+console.log(`${character} was ${action} code at ${place} on ${time}.`);
+console.log(`Suddenly, ${character} encountered ${object} and decided to fix it with single keystroke.`);
+console.log(`It turned out to be ${occasion}, and the client threw virtual celebration for the team`);
